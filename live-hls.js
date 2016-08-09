dce563714ae997fd59202d84e35eae0e12c54024
Matthew Neil <mneilweb@gmail.com>
2016-08-09 10:55:09 -0400

    Add support for cue tags (#7)
    * add cue tag support

--- live-hls.js
+++ live-hls.js
@@ -139,8 +139,11 @@ getSegmentHeader = function(lines, index) {
       header=lines[i];
     } else if (lines[i].indexOf('EXT-X-BYTERANGE') > -1) {
       byterange=lines[i];
-    } else
-    {
+    } else if (lines[i].indexOf('EXT-X-CUE') > -1) {
+      header = lines[i] + '\n' + header;
+    } else if(lines[i].indexOf('EXT-X-DISCONTINUITY') > -1 && lines[i].indexOf('EXT-X-DISCONTINUITY-SEQUENCE') === -1) {
+      header = lines[i] + '\n' + header;
+    } else {
       break;
     }
   }
@@ -529,7 +532,7 @@ master = function(request, response) {
       if (request.query.resetStream==1) {
         resetLiveStream(renditions[i]);
       }
-      
+
       //Ensure stream starts simultaneously with other renditions
       currentRendition=renditions[i];
       urlarr=currentRendition.split('?');
